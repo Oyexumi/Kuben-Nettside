f3ea47c3b122471417466ed06bf4afc20de3ca08
Korni26 <thewolron@gmail.com>
2023-01-18 09:58:46 +0100

    new patch
    skjellete til nettsiden

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("bolle")
+
+function bolle(
+
+$(document).on('scroll', function(){
+    const windowHeight = $(window).height()
+    const scrollValue = $(this).scrollTop()
+    console.log(scrollValue)
+})
+
+);
