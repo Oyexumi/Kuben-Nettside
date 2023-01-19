78816411d65dd4f7bee1ceba64ff4539a97d8ebb
Korni26 <thewolron@gmail.com>
2023-01-19 11:25:56 +0100

    ferdig animation

--- main.js
+++ main.js
@@ -11,6 +11,18 @@ console.log("bolle")
 $(document).on('scroll', function(){
     const windowHeight = $(window).height()
     const scrollValue = $(this).scrollTop()
+    const $logo = $('.logo')
+    const $fake = $('.fake')
     console.log(scrollValue)
+
+    if(scrollValue > 200){
+        $logo.addClass('topLogo')
+        $fake.addClass('fakeMargin')
+    }
+
+    if(scrollValue < 200){
+        $('.logo').removeClass('topLogo')
+        // $('.fake').removeClass('fakemargin')
+    }
 })
 
